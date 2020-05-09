48e8116c9c03babd6093e11a7593fc0f469528c1
Hicham Randrianarivo <hicham@randrianarivo.net>
2020-05-09 15:48:57 +0200

    Fix some pre-commit failed tests

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,31 +6,84 @@ import warnings
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
+def subprocess_error(msg):
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except subprocess.CalledProcessError as e:
+                warnings.warn(f"{msg}: {e}")
+
+        return wrapper
+
+    return decorator
+
+
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+@subprocess_error("Could not initialize git repository")
 def init_git():
-    try:
-        subprocess.run(["git", "init", "."])
-        subprocess.run(["git", "add", "."])
-    except subprocess.CalledProcessError:
-        warnings.warn("Can't initialize git")
+    subprocess.run(["git", "init", "."])
+    subprocess.run(["git", "add", "."])
+    subprocess.run(
+        ["git", "remote", "add", "origin", "{{ cookiecutter.project_remote }}"]
+    )
+    subprocess.run(
+        ["git", "remote", "set-url", "--push", "{{ cookiecutter.project_remote }}"]
+    )
 
 
+@subprocess_error("Could not install the module {{ cookiecutter.project_slug }}")
 def install():
-    try:
-        subprocess.run(["pip", "install", "-e", ".[dev,test,docs]"])
-    except subprocess.CalledProcessError:
-        warnings.warn("Could not install the module {{ cookiecutter.project_slug }}")
+    if "{{ cookiecutter.direnv_layout }}" == "pipenv":
+        subprocess.run(["pipenv", "install", "-e", ".[dev,test,docs]"])
+    else:
+        cmd = ["pip", "install", "-e", ".[dev,test,docs]"]
+        if "{{ cookiecutter.direnv_layout }} == conda":
+            cmd = ["conda", "run", "-n", "{{ cookiecutter.project_slug }}"] + cmd
+        subprocess.run(cmd)
 
 
+@subprocess_error("Could not initialize pre-commit")
 def pre_commit():
-    try:
-        subprocess.run(["pre-commit install"])
-        subprocess.run(["pre-commit install --hook-type push"])
-    except subprocess.CalledProcessError:
-        warnings.warn("Could not initialize pre-commit")
+    subprocess.run(["pre-commit", "install"])
+    subprocess.run(["pre-commit", "install", "--hook-type=pre-push"])
+
+
+@subprocess_error("Could not initialize direnv")
+def dir_env():
+    subprocess.run(["direnv", "allow", "."])
+    warnings.warn(
+        "If the layouts are not configured, go to "
+        "https://github.com/direnv/direnv/wiki/Python"
+    )
+
+
+def create_venv():
+    layout = "{{ cookiecutter.direnv_layout }}"
+    if layout == "anaconda":
+        cmd = [
+            "conda",
+            "create",
+            "-y",
+            "-c conda-forge",
+            "-n",
+            "{{ cookiecutter.project_slug }}",
+            "python={{ cookiecutter.pyver }}",
+        ]
+    elif layout == "pyenv":
+        cmd = [
+            "pyenv",
+            "virtualenv",
+            "{{ cookiecutter.pyver }}",
+            "{{ cookiecutter.project_slug }}",
+        ]
+    elif layout == "pipenv":
+        cmd = ["pipenv", "--python", "{{ cookiecutter.pyver }}"]
+
+    subprocess.run(cmd)
 
 
 if __name__ == "__main__":
@@ -42,5 +95,8 @@ if __name__ == "__main__":
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
 
+    create_venv()
+    dir_env()
     init_git()
     install()
+    pre_commit()
--- {{cookiecutter.project_slug}}/tests/{{cookiecutter.project_slug}}_test.py
+++ {{cookiecutter.project_slug}}/tests/{{cookiecutter.project_slug}}_test.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
 """Tests for `{{ cookiecutter.project_slug }}` package."""
+
+import importlib
+import pytest
+
+
+def test_import():
+    try:
+        importlib.import_module("{{ cookiecutter.project_slug }}")
+    except ModuleNotFoundError:
+        pytest.fail()
